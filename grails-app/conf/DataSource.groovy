3a1d6db713e00686d73a1cf7be12f72f9b18b54e
Nikos Karamolegos <ncaramolegos@gmail.com>
2014-11-26 10:43:23 +0200

    In Grails 2.3.8 and Remote database

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,15 +1,15 @@
 dataSource {
     // h2
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+//    pooled = true
+//    driverClassName = "org.h2.Driver"
+//    username = "sa"
+//    password = ""
 
     // MySQL
-//    pooled = true
-//    driverClassName = "com.mysql.jdbc.Driver"
-//    username = "root" 
-//    password = "agiler" 
+    pooled = true
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "client" 
+    password = "agiler" 
 
 }
 hibernate {
@@ -24,23 +24,23 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            url = "jdbc:h2:coreDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 //            url = "jdbc:mysql://localhost:3306/products"   
+            url = "jdbc:mysql://products.db:3306/products?useUnicode=yes&characterEncoding=UTF-8" 
         }
     }
     test {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            url = "jdbc:mysql://localhost:3306/products" 
+//            url = "jdbc:mysql://localhost:3306/commons" 
         }
     }
     production {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-//            url = "jdbc:mysql://localhost:3306/products" 
+//            url = "jdbc:mysql://localhost:3306/commons" 
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
